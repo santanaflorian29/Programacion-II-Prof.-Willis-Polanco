c0a98f28d6caae49d3d764f137bc8a6cb20f79cb
santanaflorian29 <65983176+santanaflorian29@users.noreply.github.com>
2020-08-01 23:54:53 -0400

    Firebase

---
+++ FireBase/app/src/main/java/jpinales/com/myapplication/Welcome.java
@@ -0,0 +1,24 @@
+package jpinales.com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void Logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent window = new Intent(Welcome.this, MainActivity.class);
+        startActivity(window);
+    }
+}
